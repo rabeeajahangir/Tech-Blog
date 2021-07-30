da04656628885e9a06b8d5252ee4a1a4977732d6
Rabeea Jahangir <rabeeajahangir@Rabeeas-MacBook-Air.local>
2021-07-30 16:49:44 -0400

    post requests

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -26,5 +26,45 @@ router.get('/', (req, res) => {
 });
   
 
+router.get('/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: ['id', 'post_comment', 'title', 'created_at'],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  router.post('/', (req, res) => {
+    
+    Post.create({
+      title: req.body.title,
+      post_comment: req.body.post_url,
+      user_id: req.body.user_id
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
 
 module.exports = router;
