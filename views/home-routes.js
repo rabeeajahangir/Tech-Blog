4988ecdafa7056bf8b502f3e8843140c801de529
Rabeea Jahangir <rabeeajahangir@Rabeeas-MacBook-Air.local>
2021-08-02 00:32:57 -0400

    added single post hbs and routes

--- views/home-routes.js
+++ views/home-routes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-  res.render('homepage');
+res.render('homepage', {
+  posts,
+  loggedIn: req.session.loggedIn
 });
 
 router.use('/', homeRoutes);
