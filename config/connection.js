4befcc04fa0d941df305a1fbba8f6f12b84085c1
Rabeea Jahangir <rabeeajahangir@Rabeeas-MacBook-Air.local>
2021-08-11 23:04:35 -0400

    changes to connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
+//Import sequelize constructor
 const Sequelize = require('sequelize');
 
-require('dotenv').config();
+require('dotenv').config()
 
-// create connection to our db
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    });
+//create conncection to db and pass mysql information
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
 
 module.exports = sequelize;
